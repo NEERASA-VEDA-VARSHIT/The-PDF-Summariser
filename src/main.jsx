ff059a2237e12d746b0891a74f34d358b03680a4
NEERASA VEDA VARSHIT <n.vedvarshit@gmail.com>
2025-04-20 18:37:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PDFProvider } from './context/PDFContext';
-import AppRouter from './routes'; // make sure this is the default export from routes/index.jsx
+import AppRouter from './routes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <PDFProvider>
       <AppRouter />
     </PDFProvider>
-  </React.StrictMode>
 );
