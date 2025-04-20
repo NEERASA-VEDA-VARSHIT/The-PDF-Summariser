dcc3c6960d848fbcad781686c746c4336150ec77
NEERASA VEDA VARSHIT <n.vedvarshit@gmail.com>
2025-04-20 12:21:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Footer from "./Footer"; 
+import Footer from "./components/Footer";
 import AppProvider from "./context/AppContext"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
